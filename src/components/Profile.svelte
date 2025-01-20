28e55187eb08fbed3e28bd0409ee19f0b9816205
Ruban Mikhail <m.ruban@hh.ru>
2025-01-20 23:24:20 +0100

    prepare components

---
+++ src/components/Profile.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { profile } from 'src/store/profile';
+
+    const userProfile = {name: ''};
+    const update = () => {
+            $profile = { name: 'Admin'};
+        }
+
+    profile.subscribe(value => {
+        userProfile.name = value.name;
+    });
+</script>
+
+<button on:click={update}>
+    User is {userProfile.name}
+</button>
