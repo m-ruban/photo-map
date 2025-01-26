da388c590cc465b63404c1fc77331a4390e97934
Ruban Mikhail <m.ruban@hh.ru>
2025-01-26 20:19:31 +0100

    add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-  import Counter from 'src/components/Counter.svelte'
-  import Profile from 'src/components/Profile.svelte'
+  import { Router, Link, Route } from 'svelte5-router';
+  import Counter from 'src/components/Counter.svelte';
+  import Profile from 'src/components/Profile.svelte';
+
+  export let url = "";
 </script>
 
-<main>
-  <h1>Example</h1>
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/counter">Counter</Link>
+    <Link to="/profile">Profile</Link>
+  </nav>
+  <main>
+    <div class="card">
+      <Route path="/counter" component={Counter} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/">HOME</Route>
+    </div>
+  </main>
+</Router>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <div class="card">
-    <Profile />
-  </div>
-
-</main>
