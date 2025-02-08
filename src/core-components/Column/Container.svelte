1bb13df05c590652abd8e1ae80a6ba4d4d565285
Ruban Mikhail <m.ruban@hh.ru>
2025-02-08 23:30:36 +0100

    add breakpoints

--- src/core-components/Column/Container.svelte
+++ src/core-components/Column/Container.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    let { children } = $props();
+    let { children, layout = 'default' } = $props();
 </script>
 
 <style lang="less">
     @import "src/core-components/Column/container.less";
 </style>
 
-<div class="column-container">
+<div class={`column-container column-container_layout-${layout}`}>
     {@render children?.()}
 </div>
