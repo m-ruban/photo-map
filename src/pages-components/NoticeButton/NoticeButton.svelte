de51a91df80a9f56ddb57a84349d9c578d9bec3f
Ruban Mikhail <m.ruban@hh.ru>
2025-02-15 23:59:42 +0100

    add notification button into profile page

---
+++ src/pages-components/NoticeButton/NoticeButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Button from 'src/core-components/Button/Button.svelte';
+    import NotificationsEmpty from 'src/core-components/Icon/NotificationsEmpty.svelte';
+    import NotificationsUnread from 'src/core-components/Icon/NotificationsUnread.svelte';
+
+    let { haveUnreadNotifications = true } = $props();
+</script>
+
+<Button icon={haveUnreadNotifications ? NotificationsUnread : NotificationsEmpty} type="secondary" />
--- src/pages-components/ProfileInfo/ProfileInfo.svelte
+++ src/pages-components/ProfileInfo/ProfileInfo.svelte
@@ -8,6 +8,7 @@
     import VSpace from 'src/core-components/VSpace/VSpace.svelte';
 
     import ArticleItem from 'src/pages-components/ArticleItem/ArticleItem.svelte';
+    import NoticeButton from 'src/pages-components/NoticeButton/NoticeButton.svelte';
 
     function sendRequest() {
 		console.log('sendRequest');
@@ -19,6 +20,9 @@
 </style>
 
 <Column xs="11" s="10" m="9" l="9" layout="horizontal-center">
+    <div class="notification-button">
+        <NoticeButton />
+    </div>
     <div class="profile-info">
         <div class="profile-info-avatar">
             <Avatar src="https://i.pravatar.cc/150?img=13" alt="alt" radius="small" />
@@ -52,7 +56,7 @@
                     user={{ avatar: 'https://i.pravatar.cc/150?img=13', login: 'angry_duck' }} 
                     article={{ 
                         id: '1', 
-                        images: ['https://picsum.photos/500/350', 'https://picsum.photos/500/350'], 
+                        images: ['https://picsum.photos/seed/picsum/500/350', 'https://picsum.photos/500/350'],
                         point: { lat: 1, long: 2, title: 'Сочи, ул. Ленина, д. 10'}, 
                         description: 'Мы были в Сочи и там было не плохо, но могло быть лучше...'
                     }}
@@ -64,7 +68,7 @@
                     user={{ avatar: 'https://i.pravatar.cc/150?img=13', login: 'angry_duck' }} 
                     article={{ 
                         id: '1', 
-                        images: ['https://picsum.photos/500/350', 'https://picsum.photos/500/350'], 
+                        images: ['https://picsum.photos/seed/test1/500/350'],
                         point: { lat: 1, long: 2, title: 'Сочи, ул. Ленина, д. 10'}, 
                         description: 'Мы были в Сочи и там было не плохо, но могло быть лучше...'
                     }}
@@ -76,7 +80,7 @@
                     user={{ avatar: 'https://i.pravatar.cc/150?img=13', login: 'angry_duck' }} 
                     article={{ 
                         id: '1', 
-                        images: ['https://picsum.photos/500/350', 'https://picsum.photos/500/350'], 
+                        images: ['https://picsum.photos/seed/test2/500/350'],
                         point: { lat: 1, long: 2, title: 'Сочи, ул. Ленина, д. 10'}, 
                         description: 'Мы были в Сочи и там было не плохо, но могло быть лучше...'
                     }}
@@ -88,7 +92,7 @@
                     user={{ avatar: 'https://i.pravatar.cc/150?img=13', login: 'angry_duck' }} 
                     article={{ 
                         id: '1', 
-                        images: ['https://picsum.photos/500/350', 'https://picsum.photos/500/350'], 
+                        images: ['https://picsum.photos/seed/test3/500/350'],
                         point: { lat: 1, long: 2, title: 'Сочи, ул. Ленина, д. 10'}, 
                         description: 'Мы были в Сочи и там было не плохо, но могло быть лучше...'
                     }}
@@ -100,7 +104,7 @@
                     user={{ avatar: 'https://i.pravatar.cc/150?img=13', login: 'angry_duck' }} 
                     article={{ 
                         id: '1', 
-                        images: ['https://picsum.photos/500/350', 'https://picsum.photos/500/350'], 
+                        images: ['https://picsum.photos/seed/test4/500/350'],
                         point: { lat: 1, long: 2, title: 'Сочи, ул. Ленина, д. 10'}, 
                         description: 'Мы были в Сочи и там было не плохо, но могло быть лучше...'
                     }}
