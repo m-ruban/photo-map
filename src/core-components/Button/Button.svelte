d5f10f042c15062e5480f605b1ee4c226243d7de
Ruban Mikhail <m.ruban@hh.ru>
2025-02-15 23:58:46 +0100

    add btn type secondary

--- src/core-components/Button/Button.svelte
+++ src/core-components/Button/Button.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import Text from 'src/core-components/Text/Text.svelte';
 
-    let { children, icon = null, ...otherProps } = $props()
+    let { children = null, icon = null, type = 'primary', ...otherProps } = $props()
+    let hideLabel = children === null
 </script>
 
 <style lang="less">
@@ -9,14 +10,17 @@
 </style>
 
 <button
-    class="button"
+    class={`button button_type-${type}`}
+    class:button_hile-label={hideLabel}
     type="button"
     {...otherProps}
 >
-    <Text typography="regular" size="medium" style="secondary">
-        {@render children?.()}
-    </Text>
+    {#if children}
+        <Text typography="regular" size="medium" style="secondary">
+            {@render children?.()}
+        </Text>
+    {/if}
     {#if icon}
-        <span class="icon">{@render icon?.()}</span>
+        <span class="icon" class:icon_without-label={hideLabel}>{@render icon?.()}</span>
     {/if}
 </button>
