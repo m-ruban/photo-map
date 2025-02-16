13ff7e40ff5cb678d8e0b2e756a22ed33583128a
Ruban Mikhail <m.ruban@hh.ru>
2025-02-16 20:35:28 +0100

    add custom portal

---
+++ src/core-components/Portal/Portal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { onMount, onDestroy } from "svelte";
+    export let target: HTMLElement = document.body;
+    
+    let container: HTMLDivElement;
+    let contentElement: HTMLDivElement;
+  
+    onMount(() => {
+        container = document.createElement("div");
+        target.appendChild(container);
+        container.appendChild(contentElement);
+    });
+  
+    onDestroy(() => {
+        if (container && container.parentNode) {
+            container.parentNode.removeChild(container);
+        }
+    });
+</script>
+  
+<div bind:this={contentElement}>
+    <slot />
+</div>
+  
