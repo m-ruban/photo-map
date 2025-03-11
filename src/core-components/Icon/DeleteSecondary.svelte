69276505a5c9217857ed37d72b6777da531555ab
Ruban Mikhail <m.ruban@hh.ru>
2025-03-11 22:43:07 +0100

    add some icons

---
+++ src/core-components/Icon/DeleteSecondary.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Icon from 'src/core-components/Icon/Icon.svelte';
+</script>
+
+<Icon>
+    <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3 20C2.45 20 1.97917 19.7824 1.5875 19.3472C1.19583 18.912 1 18.3889 1 17.7778V3.33333H0V1.11111H5V0H11V1.11111H16V3.33333H15V17.7778C15 18.3889 14.8042 18.912 14.4125 19.3472C14.0208 19.7824 13.55 20 13 20H3ZM13 3.33333H3V17.7778H13V3.33333ZM5 15.5556H7V5.55556H5V15.5556ZM9 15.5556H11V5.55556H9V15.5556Z" fill="#00A383"/>
+    </svg>        
+</Icon>
